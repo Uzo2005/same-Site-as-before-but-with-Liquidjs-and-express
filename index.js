9add928c39246f1658bf3371709ab82f0849f40e
Uzo2005 <igwecletus455@gmail.com>
2022-05-10 20:18:39 +0100

    Added sanity.io as Cms and hope this will work out

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const app = express();
 
 
 app.use(express.static(path.join(__dirname,'/public')));
-app.use(express.static(path.join(__dirname,'/public/login page')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/login page', 'login.html'))
+    res.sendFile(path.join(__dirname, '/public', 'index.html'))
 });
 
 const PORT  = process.env.PORT || 2022;
